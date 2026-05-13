436079ac6840fd3f6ab79e8c86126d6963e8a619
jmartins3 <jmartinsisel.pt>
2026-05-13 15:43:03 +0100

    optionals and functions

--- lecture-05-13-optionals-and-functions/src/main/java/pt/isel/mds/functional/MyComparator.java
+++ lecture-05-13-optionals-and-functions/src/main/java/pt/isel/mds/functional/MyComparator.java
@@ -9,8 +9,10 @@ public interface MyComparator<T> {
     int compare(T t1, T t2);
     
     static <T,U extends Comparable<U>> MyComparator<T> comparing(Function<T,U> key) {
-        TODO("MyComparator.comparing(Function key)");
-        return null;
+//        TODO("MyComparator.comparing(Function key)");
+//        return null;
+        return (t1, t2) ->
+                    key.apply(t1).compareTo(key.apply(t2));
 
     }
     
@@ -21,9 +23,14 @@ public interface MyComparator<T> {
         TODO("MyComparator.comparing(Function key, MyComparator keyComparator)");
         return null;
     }
-        
 
-    
+
+    /**
+     * return a comparator that comapares using this, and if the values are equals,
+     * compare the comparable values produced by key function
+     * @param key
+     * @return
+     */
     default <U extends Comparable<U>> MyComparator<T> thenComparing(Function<T,U> key) {
         TODO("MyComparator.thenComparing(Function key)");
         return null;
@@ -38,11 +45,23 @@ public interface MyComparator<T> {
         return null;
     }
 
+    /**
+     * return a comparator that comapares using this, and if the values are equals,
+     * compare them with "other" comparator
+     * @param other
+     * @return
+     */
     default MyComparator<T> thenComparing(MyComparator<T> other) {
         TODO("thenComparing(MyComparator<T> other))");
         return  null;
     }
 
+    /**
+     * returns a comparator where nulls are lower than everything else
+     * @param comparator
+     * @return
+     * @param <T>
+     */
     static <T> MyComparator<T> nullsFirst(MyComparator<T> comparator) {
         TODO("nullsFirst(MyComparator<T> other))");
         return null;
